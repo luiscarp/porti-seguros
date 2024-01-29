36717e6a5d3ed3659e6e8673e779cfa87759e3af
luiscarp <luisc.rodriguezpacheco@gmail.com>
2024-01-29 16:25:29 -0600

    speed insights

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,7 @@ import '@/styles/globals.css'
 import SEO from '../next-seo.config';
 import { DefaultSeo } from 'next-seo';
 import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from '@vercel/speed-insights/react';
 
 
 
@@ -11,6 +12,7 @@ export default function App({ Component, pageProps }) {
       <DefaultSeo {...SEO} />
       <Component {...pageProps} />
       <Analytics />
+      <SpeedInsights />
 
     </>
   )
