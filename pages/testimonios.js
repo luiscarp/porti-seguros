23733c9d13862b33299aae6718022e16c2c1b477
luiscarp <luisc.rodriguezpacheco@gmail.com>
2024-02-28 18:42:09 -0600

    testimonialSection

--- pages/testimonios.js
+++ pages/testimonios.js
@@ -16,9 +16,13 @@ function testimonios() {
 
                 <h1 className='text-center mt-1 md:mt-4 italic text-amber-500'>Opinan sobre nosotros</h1>
 
-                <h1 className='text-center mt-1 md:mt-4 italic text-amber-500'>SITIO EN CONSTRUCCION</h1>
 
 
+
+            </div>
+            <iframe height="800px" id='testimonialto-portiseguros-tag-all-light-animated' src="https://embed-v2.testimonial.to/w/portiseguros?animated=on&theme=light&shadowColor=444cf7&speed=0.75&hideDate=on&hideSource&tag=all&showHeartAnim" frameborder="0" scrolling="no" width="100%"></iframe>
+            <div className=' my-14 flex flex-row items-center justify-center'>
+                <a className=' text-white font-bold text-xl text-center p-5 rounded-lg bg-sky-600' href='https://www.google.com/search?q=portiseguros+google+maps&rlz=1C1VDKB_esMX1068MX1068&oq=portiseguros+google+maps&gs_lcrp=EgZjaHJvbWUyBggAEEUYOTIHCAEQIRigAdIBCDQ4ODNqMGo3qAIAsAIA&sourceid=chrome&ie=UTF-8#lrd=0x86ea437103ecc5b9:0xc7c0c950a9cb4843,3,,,,' target="_blank"> Escribe aqui tu opinion</a>
             </div>
             <Footer></Footer>
 
